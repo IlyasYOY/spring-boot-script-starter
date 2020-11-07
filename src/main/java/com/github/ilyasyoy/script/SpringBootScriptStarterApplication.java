5dab235ddeba39bcd1cc1095cfb122e9b2bd9c33
Ильиных Илья Сергеевич <IIlyinykh@alfabank.ru>
2020-11-07 03:25:13 +0300

    Initial commit

---
+++ src/main/java/com/github/ilyasyoy/script/SpringBootScriptStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.ilyasyoy.script;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootScriptStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootScriptStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ilyasyoy/script/SpringBootScriptStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ilyasyoy.script;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootScriptStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
